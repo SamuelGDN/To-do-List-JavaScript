c0d2cd1da658d4e18643db905326b15d5aaf1987
Samuel Gonzaga <samuelgonzagasg1@gmail.com>
2022-04-24 19:51:06 -0300

    Adcionando função de adcionar tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,25 +15,25 @@ const LINE_THROUGH = "lineThrough";
 let List = [];
 let id = 0;
 
-input.value = ''
+input.value = "";
 
 //Recuperando Lista(Se houver) do LocalStorage
-function recoverToDoList(){
-    const ListSave = localStorage.getItem('TODOLIST')
-    if(!ListSave) return;
-    List = JSON.parse(ListSave)
-    id = List.length
-    for(task of List){
-        addToDo(task.name,task.id,task.done,task.trash)
-    }
+function recoverToDoList() {
+  const ListSave = localStorage.getItem("TODOLIST");
+  if (!ListSave) return;
+  List = JSON.parse(ListSave);
+  id = List.length;
+  for (task of List) {
+    addToDo(task.name, task.id, task.done, task.trash);
+  }
 }
 recoverToDoList();
 
 //Adcionando Função de apagar localStorage
-clear.addEventListener('click',() =>{
-    localStorage.setItem('TODOLIST',"")
-    location.reload();
-})
+clear.addEventListener("click", () => {
+  localStorage.setItem("TODOLIST", "");
+  location.reload();
+});
 
 //Adcionando a data atual
 dateElement.innerHTML = today.toLocaleDateString("pt-BR", options);
@@ -60,48 +60,60 @@ function addToDo(todo, id, done, trash) {
 input.addEventListener("keypress", (e) => {
   if (e.keyCode == 13) {
     if (!input.value) return;
-    addToDo(input.value,id,false,false);
+    addToDo(input.value, id, false, false);
     List.push({
-        name: input.value,
-        id: id,
-        done: false,
-        trash: false
-    })
+      name: input.value,
+      id: id,
+      done: false,
+      trash: false,
+    });
     input.value = "";
     id++;
-    localStorage.setItem('TODOLIST',JSON.stringify(List))
+    localStorage.setItem("TODOLIST", JSON.stringify(List));
+    input.focus();
   }
 });
 
-// btnAddTask.addEventListener("click", (e) => {
-//   if (!input.value) return;
-//   addToDo(input.value);
-//   input.value = "";
-//   input.focus();
-// });
+btnAddTask.addEventListener("click", (e) => {
+  if (!input.value) return;
+  addToDo(input.value, id, false, false);
+  List.push({
+    name: input.value,
+    id: id,
+    done: false,
+    trash: false,
+  });
+  input.value = "";
+  id++;
+  localStorage.setItem("TODOLIST", JSON.stringify(List));
+  input.focus();
+});
 
 //Concluindo a tarefa
 
-function completeToDo(el){
-    el.classList.toggle(CHECK)
-    el.classList.toggle(UNCHECK)
-    el.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH)
-    List[el.id].done = List[el.id].done ? false : true
+function completeToDo(el) {
+  el.classList.toggle(CHECK);
+  el.classList.toggle(UNCHECK);
+  el.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  List[el.id].done = List[el.id].done ? false : true;
 }
 
 //Removendo a tarefa
 
-function removeToDo(el){
-    el.parentNode.remove()
-    List[el.id].trash = true
+function removeToDo(el) {
+  el.parentNode.remove();
+  List[el.id].trash = true;
 }
 
 //selescionando dinamicamente os elementos clicados
 
-list.addEventListener('click',(event) =>{
-    const el = event.target
-    if(el.classList.contains('co')){completeToDo(el)}
-    if(el.classList.contains('de')){removeToDo(el)}
-    localStorage.setItem('TODOLIST',JSON.stringify(List))
-})
-
+list.addEventListener("click", (event) => {
+  const el = event.target;
+  if (el.classList.contains("co")) {
+    completeToDo(el);
+  }
+  if (el.classList.contains("de")) {
+    removeToDo(el);
+  }
+  localStorage.setItem("TODOLIST", JSON.stringify(List));
+});
