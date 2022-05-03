e49ccd0f9e4b7cc8bd64fc0a99ba028e612931bf
Samuel Gonzaga <samuelgonzagasg1@gmail.com>
2022-05-02 22:25:17 -0300

    Adcionando Favicon e darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function myEscope(){
+function myEscope() {
   const clear = document.querySelector(".clear");
   const dateElement = document.getElementById("date");
   const list = document.getElementById("list");
@@ -6,20 +6,20 @@ function myEscope(){
   const today = new Date();
   const options = { weekday: "long", month: "long", day: "numeric" };
   const btnAddTask = document.getElementById("addTask");
-  
+
   //Nome das classes
   const CHECK = "fa-check-circle";
   const UNCHECK = "fa-circle-thin";
   const LINE_THROUGH = "lineThrough";
-  
+
   //variaveis
   let List = [];
   let id = 0;
   let isedit = false;
   let editid;
-  
+
   input.value = "";
-  
+
   //Recuperando Lista(Se houver) do LocalStorage
   function recoverToDoList() {
     const ListSave = localStorage.getItem("TODOLIST");
@@ -31,16 +31,16 @@ function myEscope(){
     }
   }
   recoverToDoList();
-  
+
   //Adcionando Função de apagar localStorage
   clear.addEventListener("click", () => {
     localStorage.setItem("TODOLIST", "");
     location.reload();
   });
-  
+
   //Adcionando a data atual
   dateElement.innerHTML = today.toLocaleDateString("pt-BR", options);
-  
+
   //Função para adcionar a tarefa
   function addToDo(todo, id, done, trash) {
     if (trash) return;
@@ -57,7 +57,7 @@ function myEscope(){
      `;
     list.insertAdjacentHTML(position, item);
   }
-  
+
   //Adcionando a tarefa ao pressionar "enter" ou clicar no botao
   input.addEventListener("keypress", (e) => {
     if (e.keyCode == 13) {
@@ -69,15 +69,15 @@ function myEscope(){
         localStorage.setItem("TODOLIST", JSON.stringify(List));
       } else {
         isedit = false;
-        List[editid].name = input.value
-        list.querySelector(`#task${editid}`).innerText = input.value
+        List[editid].name = input.value;
+        list.querySelector(`#task${editid}`).innerText = input.value;
         localStorage.setItem("TODOLIST", JSON.stringify(List));
       }
       input.value = "";
       input.focus();
     }
   });
-  
+
   btnAddTask.addEventListener("click", () => {
     if (!input.value) return;
     if (!isedit) {
@@ -87,48 +87,48 @@ function myEscope(){
       localStorage.setItem("TODOLIST", JSON.stringify(List));
     } else {
       isedit = false;
-      List[editid].name = input.value
-      list.querySelector(`#task${editid}`).innerText = input.value
+      List[editid].name = input.value;
+      list.querySelector(`#task${editid}`).innerText = input.value;
       localStorage.setItem("TODOLIST", JSON.stringify(List));
     }
     input.value = "";
     input.focus();
   });
-  
+
   //Concluindo a tarefa
-  
+
   function completeToDo(el) {
     el.classList.toggle(CHECK);
     el.classList.toggle(UNCHECK);
     el.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
     List[el.id].done = List[el.id].done ? false : true;
   }
-  
+
   //Removendo a tarefa
-  
+
   function removeToDo(el) {
     el.parentNode.remove();
     List[el.id].trash = true;
-    if(isedit){
-      if(editid == el.id){
-        isedit = false
-        input.value = ''
+    if (isedit) {
+      if (editid == el.id) {
+        isedit = false;
+        input.value = "";
       }
     }
   }
-  
+
   //Editando a tarefa
-  
+
   function editTodo(el) {
     isedit = true;
-    editid = el.id
+    editid = el.id;
     const text = el.parentNode.querySelector(".text").innerText;
     input.value = text;
     input.focus();
   }
-  
+
   //selescionando dinamicamente os elementos clicados
-  
+
   list.addEventListener("click", (event) => {
     const el = event.target;
     if (el.classList.contains("co")) {
@@ -142,7 +142,34 @@ function myEscope(){
     }
     localStorage.setItem("TODOLIST", JSON.stringify(List));
   });
+
+  //Adcionando modo noturno
+  const themeButton = document.querySelector("#theme-button");
+  const darktheme = "dark-theme";
+  const icon = "fa-sun-o";
+
+  themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darktheme);
+    if (document.body.classList.contains(darktheme)) {
+      localStorage.setItem("darkmode", "true");
+      themeButton.classList.remove('fa-moon-o');
+      themeButton.classList.add(icon);
+    } else {
+      localStorage.setItem("darkmode", "false");
+      themeButton.classList.remove(icon);
+      themeButton.classList.add('fa-moon-o');
+    }
+  });
+
+  if (localStorage.getItem("darkmode") === "true") {
+    document.body.classList.add(darktheme);
+    themeButton.classList.remove('fa-moon-o');
+    themeButton.classList.add(icon);
+  } else {
+    document.body.classList.remove(darktheme);
+    themeButton.classList.remove(icon);
+    themeButton.classList.add('fa-moon-o');
+  }
 }
 
 myEscope();
-
